c6a04b8f0b3ad3970e1819740bacc1e6b0641ce5
JacksonPaulHeck <jacksonheck1@gmail.com>
2021-09-27 00:12:55 -0500

    Implementation of the forward iterator in the List Class.

--- src/AlgoList.h
+++ src/AlgoList.h
@@ -1,7 +1,26 @@
 #include "AlgoListNode.h"
+#include <iterator> // For std::forward_iterator_tag
+
 
 template <typename T>
 class AlgoList{
+    struct Iterator {
+      using iterator_category = std::forward_iterator_tag;
+      using difference_type   = std::ptrdiff_t;
+      using value_type        = AlgoListNode<T>;
+      using pointer           = AlgoListNode<T>*;
+      using reference         = AlgoListNode<T>&;
+      
+      Iterator(pointer ptr) : m_ptr(ptr) {}
+      reference operator*() const { return *m_ptr; }
+      pointer operator->() { return m_ptr; }
+      Iterator& operator++() { m_ptr=m_ptr->next; return *this; }  
+      Iterator operator++(T) { Iterator tmp = *this; ++(*this); return tmp; }
+      friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
+      friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; }; 
+    private: 
+      pointer m_ptr;
+  };
 public:
 	AlgoList();
 	~AlgoList();
